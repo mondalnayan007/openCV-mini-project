69f64385cc7d01401023dab1fcccda82106618fb
Nayan Mondal <n007kingkong@gmail.com>
2026-03-15 01:40:01 +0600

    gray scale conversion is done

---
+++ app.py
@@ -0,0 +1,21 @@
+import cv2 
+
+image = cv2.imread("D:/Python/Opencv/mini project/butterfly.png")
+
+# cv2.imshow("Image is showing" , image)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+if image is not None :
+    print(" Press 1 to Convert to GrayScale----> \n Press 2 to Resize image----> \n Press 3 to Crop Image---> \n Press 4 to Flip Image---> \n Press 5 to Rotate Image-----> \n Press 6 to Save Image---->")
+    operation = input("Choose your Operation  : ")
+    if operation == "1":
+        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        cv2.imshow("GrayScale image is showing" , gray)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    
+
+else:
+    print("Invalid image path check and retry !!!")
